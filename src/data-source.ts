f9f10f9b994f9b35dde637a622f5eaba2b286b0d
Pedro Silva <pedrosilvacwb@gmail.com>
2023-01-08 09:41:35 -0300

    teste

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,28 +1,34 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import path from 'path';
 import 'dotenv/config';
 
 const nodeEnv: string = process.env.NODE_ENV;
 
-const AppDataSource =
-  nodeEnv === 'production'
-    ? new DataSource({
-        type: 'postgres',
-        url: process.env.DATABASE_URL,
-        entities: [path.join(__dirname, './entities/**.{js,ts}')],
-        migrations: [path.join(__dirname, './migrations/**{js,ts}')],
-      })
-    : new DataSource({
-        type: 'postgres',
-        host: process.env.PGHOST,
-        port: +process.env.PGPORT,
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        logging: true,
-        synchronize: false,
-        entities: [path.join(__dirname, './entities/**.{js,ts}')],
-        migrations: [path.join(__dirname, './migrations/**{js,ts}')],
-      });
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, './entities/*.{js,ts}');
+  const migrationsPath: string = path.join(__dirname, './migrations/*.{js,ts}');
 
-export default AppDataSource;
+  const nodeEnv: string = process.env.NODE_ENV;
+
+  if (nodeEnv === 'production') {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+  return {
+    type: 'postgres',
+    host: process.env.PGHOST,
+    port: parseInt(process.env.PGPORT),
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+export const AppDataSource = new DataSource(dataSourceConfig());
